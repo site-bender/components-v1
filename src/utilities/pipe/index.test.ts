823ff750b860f2661509cf52624eddebd3763750
Charles F. Munat <charles.munat@gmail.com>
2023-11-01 14:13:41 +1300

    0.1.48

--- src/utilities/pipe/index.test.ts
+++ src/utilities/pipe/index.test.ts
@@ -1,9 +1,21 @@
 import pipe from "."
 
-const add = (x: number) => (y: number): number => x + y
-const multiply = (x: number) => (y: number): number => x * y
-const subtract = (x: number) => (y: number): number => y - x
-const divide = (x: number) => (y: number): number => y / x
+const add =
+	(x: number) =>
+	(y: number): number =>
+		x + y
+const multiply =
+	(x: number) =>
+	(y: number): number =>
+		x * y
+const subtract =
+	(x: number) =>
+	(y: number): number =>
+		y - x
+const divide =
+	(x: number) =>
+	(y: number): number =>
+		y / x
 
 test("[pipe] accepts array of functions and returns a function that calls them sequentially", () => {
 	expect(pipe([add(5), multiply(4), subtract(10), divide(2)])(0)).toBe(5)
